3daec1f9a8c7b53b0ac8beedf824631a1dc4b409
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs.uni-erlangen.de>
2017-01-03 17:49:14 +0100

    go down to 64kbs

--- encode_howler.sh
+++ encode_howler.sh
@@ -1,2 +1,2 @@
 afconvert howler/howler.wav howler/howler.caf -d 0 -f caff --soundcheck-generate
-afconvert howler/howler.caf -d aac -f m4af -u pgcm 2 --soundcheck-read -b 96000 -q 127 -s 2 howler/howler.m4a
+afconvert howler/howler.caf -d aac -f m4af -u pgcm 2 --soundcheck-read -b 64000 -q 127 -s 2 howler/howler.m4a
